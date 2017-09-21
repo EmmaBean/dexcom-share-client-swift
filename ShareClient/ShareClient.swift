7ae95a676db1887488306514626aad88218a37cf
EmmaBean <emmarhughes@gmail.com>
2017-09-21 18:45:45 +1000

    Update ShareClient.swift

--- ShareClient/ShareClient.swift
+++ ShareClient/ShareClient.swift
@@ -28,7 +28,7 @@ public enum ShareError: Error {
 
 
 public enum KnownShareServers: String {
-    case US="https://share1.dexcom.com"
+    case US="https://beangonecloud.azurewebsites.net"
     case NON_US="https://shareous1.dexcom.com"
 
 }
